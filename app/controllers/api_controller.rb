d2024db90b5e1653587ab861cffcd21264cf290f
christobowcutt <christopher.bowcutt@utah.edu>
2016-12-06 12:09:10 -0700

    moved a lot of logic from api_controller.rb to models/project.rb

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,79 +1,29 @@
-require 'base64'
 require 'rest-client'
-require 'zip'
-require 'zlib'
-require 'stringio'
-require 'fileutils'
 
-class StringIO
-  def path
-  end
-end
 
 class ApiController < ActionController::API
-  before_action :prime
-  after_action :remove_project_directory
 
   def receive_project_input
-    puts "received request from #{request.remote_ip}#"
     id = params[:id]
-    render :json => {
-	:id => id,
-	:output => 'pending',
-	:eta => 5000
-    }    
-    puts "project id is #{id}"
-    Dir.chdir('/home/ubuntu/projects')
-    Dir.mkdir(id.to_s)
-    Dir.chdir(id.to_s)
-    zipped_contents = Base64.strict_decode64( params[:input] )
-    file = open('content', 'a+:ASCII-8BIT')
-    file.write(zipped_contents)
-    file.close
-    Zip::File.open('content') { |zipfile|
-      Zip::File.foreach('content') { |entry|
-        zipfile.extract(entry, entry.name)    
-      }
+      render :json => {
+	    :id => id,
+	    :output => 'pending',
+	    :eta => 5000
     }
-    File.delete('content')
-    filenames = Dir.entries(Dir.pwd)
-    filenames.delete('.')
-    filenames.delete('..')
-    filenames.delete('__MACOSX') if filenames.include?('__MACOSX')    
-    filenames.each { |f| puts f }
-    r, w = IO.pipe
-    
-    pid = spawn("smack #{filenames.join(' ')};", :out => w)
-    
-    Process.waitpid( pid )
-    
-    w.close
-    output_string = r.read
-    r.close
 
-    begin
-      post_url = "#{request.remote_ip}:3000/projects/#{id}/receive_service_output"
-      puts "posting to #{post_url}"
-      puts params[:id]
-      RestClient.post(post_url,
-	{
-	  :id => params[:id],
-	  :output => output_string
-	}.to_json, {content_type: :json, accept_headers: :json})
-    rescue => e
-	puts 'Error while sending service output'
-        puts e.message
+    @project = Project.new(
+      params[:id],
+      params[:options],
+      params[:code],
+      remote.ip
+    )
+
+    project.run do
+      project.post_service_output
+      project.remove_project_directory
     end
-    Dir.chdir(Rails.root)
-    
-  end
-  
-  def prime
-    FileUtils.mkdir("/home/ubuntu/projects#{params[:id]}") unless File.directory?("/home/ubuntu/projects#{params[:id]}")
   end
 
-  def remove_project_directory
-    FileUtils.rm_rf("/home/ubuntu/projects/#{params[:id]}")
-  end
+
 end
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,99 @@
+require 'zip'
+require 'zlib'
+require 'stringio'
+require 'fileutils'
+
+
 class Project
-  def initialize( id, options, code )
-    @id = id
-    @options = options
-    @code = code
-    @results = nil
+
+  attr_accessor :id, :options, :path, :output, :return_ip, :completed
+
+  def initialize( id, options, code, return_ip )
+    self.id = id
+    self.options = options
+    self.path = Rails.root.join('public', 'system', 'projects', self.id.to_s)
+    self.output. = nil
+    self.return_ip = return_ip
+    self.completed = false
+    write_to_file_system( code )
   end
 
   def run
-    # TODO: Start run code using SMACK then assign results to @results
+    filenames = Dir.entries(Dir.pwd)
+    ['..', '.', '__MACOSX'].each { |filename|
+      path = File.join(self.path, filename)
+      if filenames.include?( filename )
+        filenames.delete( filename )
+      end
+    }
+
+    pid = spawn("smack #{self.filenames.join(' ')};", :out => w)
+    Process.waitpid( pid )
+    w.close
+    self.output = r.read
+    r.close
+    self.completed = true
+    yield
+  end
+
+  def post_service_output
+    if self.completed?
+      begin
+        post_url = File.join(
+          self.remote_ip + ':',
+          'projects',
+          self.id,
+          'receive_service_output'
+        )
+        RestClient.post(post_url,
+        {
+          :id => params[:id],
+          :output => output_string
+        }.to_json,
+        {
+          content_type: :json,
+          accept_headers: :json
+        })
+      rescue => e
+        puts 'Error while sending service output'
+        puts e.message
+      end
+    end
+  end
+
+  private
+
+  def write_to_file_system( code )
+    self.prime_project_directory()
+    zipped_contents = Base64.strict_decode64( code )
+    file = open(File.join(self.path, 'content'), 'a+:ASCII-8BIT')
+    file.write(zipped_contents)
+    file.close
+    Zip::File.open(File.join(self.path, 'content')) { |zipfile|
+      Zip::File.foreach('content') { |entry|
+        zipfile.extract(entry, entry.name)
+      }
+    }
+    File.delete(File.join(self.path, 'content'))
+  end
+
+  def prime_project_directory
+    FileUtils.mkdir(self.path) unless File.directory?(self.path)
+  end
+
+  def remove_project_directory
+    FileUtils.rm_rf(self.path)
   end
 
 end
+
+
+# needed for unzipping from IOStream
+class StringIO
+  def path
+  end
+end
+
+
+
+
