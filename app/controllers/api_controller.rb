1b9e8bd78efe27d7186697078a402a8c2f59e3a6
christobowcutt <christopher.bowcutt@utah.edu>
2017-01-12 16:58:24 -0700

    updated api_controller_test

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,26 +5,26 @@ class ApiController < ActionController::API
 
   def receive_project_input
   
-  Thread.new do 
-    @project = Project.new(
-      params[:id].to_s,
-      params[:options],
-      Base64.strict_decode64( params[:input] ),
-      request.remote_ip
-    )
+    Thread.new do 
+      @project = Project.new(
+        params[:id].to_s,
+        params[:options],
+        Base64.strict_decode64( params[:input] ),
+        request.remote_ip
+      )
 
-    @project.run
-    @project.post_service_output
-    @project.remove_project_directory
-    FileUtils.rm_rf(params[:id].to_s)
-    Dir.chdir(Rails.root)   
-  end
+      @project.run
+      @project.post_service_output
+      @project.remove_project_directory
+      FileUtils.rm_rf(params[:id].to_s)
+      Dir.chdir(Rails.root)   
+    end
 
-  render :json => {
-    :id => params[:id].to_s,
-    :output => 'pending',
-    :eta => 5000
-     }
+    render :json => {
+      :id => params[:id].to_s,
+      :output => 'pending',
+      :eta => 5000
+       }
 
   end
 end
