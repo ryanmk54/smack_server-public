eeb9abbac8aab14bf62aa770b307b7998c80d144
Ubuntu <ubuntu@ip-172-31-3-147.us-west-1.compute.internal>
2017-01-12 23:32:58 +0000

    controller now sends ETA response immediiately after recieving a request

--- startbg.sh
+++ startbg.sh
@@ -1,3 +1,7 @@
 !#/bin/bash
 rm -rf ~/projects/*
+rm nohup.sh
+export BOOGIE="mono /home/ubuntu/boogie/Binaries/Boogie.exe"
+export CORRAL="mono /home/ubuntu/corral/bin/Release/corral.exe"
+export LOCKPWN="mono /home/ubuntu/lockpwn/Binaries/lockpwn.exe"
 nohup rails server -b ec2-52-53-187-90.us-west-1.compute.amazonaws.com &
